30043ac6f77b7a67d300fc4c0e0a3f84be6a31da
mihsan <mihsan191@gmail.com>
2017-10-05 22:02:50 +1100

    feat: Add initial store declaration

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,7 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './style/index.css';
 import App from './components/App';
+import initialState from './reducers/initialState';
+import configureStore from './store/configureStore';
 import registerServiceWorker from './registerServiceWorker';
+
+const store = configureStore(initialState);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
---
+++ frontend/src/store/configureStore.js
@@ -0,0 +1,9 @@
+import { createStore } from 'redux';
+import rootReducer from '../reducers/rootReducer';
+
+export default (initialState) => {
+  return createStore(
+    rootReducer,
+    initialState
+  );
+};
