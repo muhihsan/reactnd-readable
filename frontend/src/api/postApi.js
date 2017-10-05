6b63d5356b9b8fc25c03e03c45cb0fa6783030d3
mihsan <mihsan191@gmail.com>
2017-10-05 23:03:29 +1100

    feat: Add API to get all posts

--- frontend/src/api/postApi.js
+++ frontend/src/api/postApi.js
@@ -10,4 +10,16 @@ export default class postApi {
     .then((res) => res.json())
     .then(({ posts }) => posts);
   }
+  
+  static getAllPosts = () => {
+    return fetch(
+      `http://localhost:3001/posts`,
+      {
+        headers: { 'Authorization': 'whatever-you-want' },
+        method: 'GET',
+      }
+    )
+    .then((res) => res.json())
+    .then(({ posts }) => posts);
+  }
 };
