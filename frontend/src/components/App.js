c118f26ad132bf0020fce024a85607029c0bdf0e
mihsan <mihsan191@gmail.com>
2017-10-07 14:53:23 +1100

    feat: Add HomeComponent to show homepage information

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { Route, Switch } from 'react-router';
 import { connect } from 'react-redux';
-import logo from '../icons/logo.svg';
+import Home from './Home';
 import '../style/App.css';
 
 const ConnectedSwitch = connect(state => ({
@@ -12,17 +12,7 @@ class App extends Component {
   render() {
     return(
       <ConnectedSwitch>
-        <Route exact path="/" component={(props) => (
-          <div className="App">
-            <header className="App-header">
-              <img src={logo} className="App-logo" alt="logo" />
-              <h1 className="App-title">Welcome to React</h1>
-            </header>
-            <p className="App-intro">
-              To get started, edit <code>src/App.js</code> and save to reload.
-            </p>
-          </div>
-        )} />
+        <Route exact path="/" component={(props) => ( <Home /> )} />
       </ConnectedSwitch>
     );
   }
