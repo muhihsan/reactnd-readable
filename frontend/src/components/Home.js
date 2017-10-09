7d8e9877717c2919800b9dfae5b5eaf083e4c48b
mihsan <mihsan191@gmail.com>
2017-10-09 18:41:07 +1100

    feat: Show list of categories on home page

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 import { bindActionCreators } from 'redux';
 import * as categoryActions from '../actions/categoryAction';
 import * as postActions from '../actions/postAction';
@@ -12,15 +13,51 @@ class Home extends Component {
   }
 
   render = () => {
+    const { 
+      posts: { 
+        entities: posts, 
+        result: listPosts 
+      },
+      categories: {
+        entities: categories,
+        result: listCategories
+      }
+    } = this.props;
+
     return(
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="App-intro">
+          {listCategories && listCategories.length > 0 && (
+            <ul>
+              {listCategories.map(name => 
+                <ul key={name}>
+                  <Link to={`/${name}/posts/`}>{categories[name].name}</Link>
+                </ul>
+              )}
+            </ul>
+          )}
+          {(!listCategories || listCategories === 0) && (
+            <div>List of posts for category will be here</div>
+          )}
+        </div>
+        <div className="App-intro">
+          {listPosts && listPosts.length > 0 && (
+            <ul>
+              {listPosts.map(id => 
+                <ul key={id}>
+                  <Link to={`/posts/${id}`}>{posts[id].title}</Link>
+                </ul>
+              )}
+            </ul>
+          )}
+          {(!listPosts || listPosts === 0) && (
+            <div>List of posts for category will be here</div>
+          )}
+        </div>
       </div>
     );
   };
