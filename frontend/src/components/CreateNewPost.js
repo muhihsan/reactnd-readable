a3da5a566675e69be3e56ae1c8b847c8bfca68e9
mihsan <mihsan191@gmail.com>
2017-10-09 19:36:28 +1100

    feat: Create new post object upon clicking create button in CreateNewPost component

--- frontend/src/components/CreateNewPost.js
+++ frontend/src/components/CreateNewPost.js
@@ -1,11 +1,49 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import uuidv4 from 'uuid/v4';
+import * as postActions from '../actions/postAction'
 
 class CreateNewPost extends Component {
+  state = {
+    title: '',
+    body: '',
+    author: '',
+    category: ''
+  }
+
+  createNewPost = () => {
+    const post = {
+      id: uuidv4(),
+      timestamp: Date.now(),
+      title: this.state.title,
+      body: this.state.body,
+      author: this.state.author,
+      category: this.state.category
+    };
+    console.log(post);
+  }
+
   render = () => {
     return(
-      <div>CreateNewPost</div>
+      <div>
+        CreateNewPost
+        <div>
+          <button onClick={this.createNewPost}>Create New Post</button>
+        </div>
+      </div>
     );
   }
 }
 
-export default CreateNewPost;
+const mapStateToProps = (state) => state;
+
+const mapDispatchToProps = (dispatch) => ({
+  actions: bindActionCreators({
+      ...postActions
+    },
+    dispatch
+  )
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(CreateNewPost);
