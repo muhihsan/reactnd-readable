3422b83e4a3790b0936c51b96f49d758acab24fa
mihsan <mihsan191@gmail.com>
2017-10-09 23:35:26 +1100

    feat: Show form to create post and create post object when clicking on create post button

--- frontend/src/components/CreateNewPost.js
+++ frontend/src/components/CreateNewPost.js
@@ -5,29 +5,32 @@ import uuidv4 from 'uuid/v4';
 import * as postActions from '../actions/postAction'
 
 class CreateNewPost extends Component {
-  state = {
-    title: '',
-    body: '',
-    author: '',
-    category: ''
-  }
-
   createNewPost = () => {
     const post = {
       id: uuidv4(),
       timestamp: Date.now(),
-      title: this.state.title,
-      body: this.state.body,
-      author: this.state.author,
-      category: this.state.category
+      title: this.title.value,
+      body: this.body.value,
+      author: this.author.value,
+      category: this.category.value
     };
     console.log(post);
   }
 
   render = () => {
+    const { categories: {entities: categories, result: listCategory} } = this.props;
+
     return(
       <div>
         CreateNewPost
+        <div>Title <input type="text"  ref={node => this.title = node} /></div>
+        <div>Body <input type="text"  ref={node => this.body = node} /></div>
+        <div>Author <input type="text"  ref={node => this.author = node} /></div>
+        <div>Category 
+          <select ref={node => this.category = node}>
+            {listCategory.map(key => <option key={key} value={categories[key].path}>{categories[key].name}</option>)}
+          </select>
+        </div>
         <div>
           <button onClick={this.createNewPost}>Create New Post</button>
         </div>
