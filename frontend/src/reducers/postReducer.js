7bca33cc4b5fefbdce1ba3b09a8ab5baf5603a49
mihsan <mihsan191@gmail.com>
2017-10-16 21:48:08 +1100

    feat: Update store post after upvoting or downvoting

--- frontend/src/reducers/postReducer.js
+++ frontend/src/reducers/postReducer.js
@@ -5,6 +5,7 @@ const initialState = null;
 export default (state = initialState, action) => {
   switch(action.type) {
     case Types.GET_POST_SUCCESS:
+    case Types.CHANGE_POST_VOTE_SUCCESS:
       return action.post;
     default:
       return state;
