99e1c663678b31dc42e1e93d7b088c9d551cb46f
mihsan <mihsan191@gmail.com>
2017-10-16 22:53:05 +1100

    feat: Remove deleted comment from comments store

--- frontend/src/reducers/commentsReducer.js
+++ frontend/src/reducers/commentsReducer.js
@@ -36,6 +36,17 @@ export default (state = initialState, action) => {
           ...state.result
         ]
       };
+    case types.DELETE_COMMENT_FOR_POST_SUCCESS:
+      const result = state.result.filter(comment => comment !== action.comment.id);
+      return {
+        entities: Object.keys(state.entities)
+                  .filter(key => result.includes(key))
+                  .reduce((entities, key) => {
+                    entities[key] = state.entities[key];
+                    return entities;
+                  }, {}),
+        result: result
+      };
     default:
       return state;
   }
