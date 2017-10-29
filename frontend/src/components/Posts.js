4fd35afbf4b8f3317ec2139c83b7e822f92aa9a9
mihsan <mihsan191@gmail.com>
2017-10-29 11:31:36 +1100

    feat: Use material card to show posts on home page

--- frontend/src/components/Posts.js
+++ frontend/src/components/Posts.js
@@ -3,6 +3,8 @@ import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import * as postActions from '../actions/postAction';
+import {Card, CardActions, CardHeader, CardMedia, CardTitle, CardText} from 'material-ui/Card';
+import Divider from 'material-ui/Divider';
 
 class Posts extends Component {
   componentDidMount = () => {
@@ -25,14 +27,29 @@ class Posts extends Component {
     return (
       <div>
         {listPosts && listPosts.length > 0 && (
-          <ul>
+          <div>
             {listPosts.map(id =>
-              <li key={id}>
-                <Link to={`/${posts[id].category}/${id}`}>{posts[id].title}</Link>
-                <button value={id} onClick={this.deletePost}>Delete</button>
-              </li>
+              <div key={id}>
+                <Card>
+                  <CardHeader
+                    title={posts[id].title}
+                    subtitle={posts[id].category}
+                  />
+                  <Divider />
+                  <CardActions>
+                    <span>{posts[id].author}</span>
+                    <i className="material-icons">thumb_up</i>
+                    <i className="material-icons">thumb_down</i>
+                    <i className="material-icons">mode_edit</i>
+                    <i className="material-icons">delete</i>
+                  </CardActions>
+                  {/* <Link to={`/${posts[id].category}/${id}`}>{posts[id].title}</Link>
+                  <button value={id} onClick={this.deletePost}>Delete</button> */}
+                </Card>
+                <br />
+              </div>
             )}
-          </ul>
+          </div>
         )}
         {(!listPosts || listPosts === 0) && (
           <div>List of posts for category will be here</div>
